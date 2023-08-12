3bb2c99140ab898a3f37559a40106dee13217aab
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2023-08-12 15:03:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get(''))
 API_HASH = environ.get('')
-BOT_TOKEN = environ.get('5977707387:AAHwoVtIjETuAoH4_u0XJgKa_1y9rUQ8ZQM')
+BOT_TOKEN = environ.get('')
 
 
 
